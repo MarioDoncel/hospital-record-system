d4d9ed16fb3488f649a0a6b73cabee13639f980f
Matheus Reis <77196447+matheusxreis@users.noreply.github.com>
2021-11-06 21:11:20 -0300

    [Mudanças na function LoginIsRIght]
    Ao que vi tudo certo, só alterei a comparação do input para os números da tabela ASCII. Da outra forma estava dando erro. 
    E também adicionei as libs diretamente nessa função.

--- code/LoginIsRight.c
+++ code/LoginIsRight.c
@@ -1,38 +1,71 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 _Bool LoginIsRight(char AdminLogin[], char AdminPassword[]){
+
+
     int verificator = 0;
     char input;
+
     char employeeLogin[15] = "admin";
+
     char typedLogin[15];
+
     char employeePassword[15] = "12345";
+
     char typedPassword[15];
 
+
+
+
     while (verificator != 1)
+
     {
+
         printf("\nDigite o usuário: \n");
-        scanf("%s", &typedLogin);
+
+        scanf("%s", typedLogin);
+
         printf("Digite a senha: \n");
-        scanf("%s", &typedPassword);
-    
-    if (strcmp(typedLogin, employeeLogin) == 0 && strcmp(typedPassword, employeePassword) == 0)
-        {
+
+        scanf("%s", typedPassword);
+
+
+    if (strcmp(typedLogin, employeeLogin) == 0 && strcmp(typedPassword, employeePassword) == 0){
+
             printf("\n\nLOGADO!\n\n");
             verificator = 1;
-            system("PAUSE");
+
+            //system("PAUSE");
+
             fflush(stdin);
-        }
-           
-        else
-        {
+
+        
+        }else{
+
             printf("\n\nDADOS INVALIDOS.\n\n");
+
             printf("Deseja tentar novamente? (S/N) \n");
-            scanf("%c", &input);
-            if(input == "N" || input == "n"){
-                system("cls");
-                return 0;
+
+            scanf("%s", &input);
+        
+
+            if(input==110||input==78){  //n e N na tabela ASCII
+               // system("cls");
+                    return 0;
+
             }
-            system("PAUSE");
-            system("cls");
+
+        
+            //system("PAUSE"); --------------------> WINDOWS
+
+            //system("cls") ------------------------> WINDOWS
+
         }             
+
     }
+
         return 1;
-}
+
+
+} 
